8121ddff634a378a69f674fdbeb0f808e3f88cb5
Eoghan McNulty <eoghan.mcnulty@gmail.com>
2012-03-09 17:09:20 +0000

    changes: salt and encrypted_password to User model

--- db/migrate/20120309163349_create_users.rb
+++ db/migrate/20120309163349_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :salt
+      t.string :encrypted_password
 
       t.timestamps
     end
